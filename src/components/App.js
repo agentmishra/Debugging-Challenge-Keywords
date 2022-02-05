82775a335f09000b7f62d389e2d9558a4d54ae9e
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-02-06 04:52:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,21 @@ import '../styles/App.css';
 
 const App = () => {
 
-  let [count, setCount] = useState(0)
+  let [count, setCount] = useState(0);
   
   const myAlert = () => {
     alert('cant edit it'); 
-  }
+  };
   const myIncre = () => { 
     setCount(count + 1); 
-  }
+  };
 
   return (
     <div className="ball">
       <h1 className="count" ondoubleclick={myAlert}>{count}</h1>
       <button className="increment-button" onclick={myIncre}>Increment</button>
     </div>
-  )
+  );
 }
 
 
