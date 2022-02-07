dba093a66dc1935deedad307af8aa6295f1d3adf
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-02-08 02:24:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
+
 import React, { useEffect, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-
-  let [count, setCount] = useState(0);
-  
-  
+  const [count, setCount] = useState(0);
 
   return (
     <div className="ball">
-      <h1 className="count" ondoubleclick={() => alert('cant edit it') }>{count}</h1>
-      <button className="increment-button" onclick={() => setCount(count + 1) }>Increment</button>
+      <h1
+        className="count"
+        onDoubleClick={() => {
+          alert("cant edit it");
+        }}
+      >
+        {count}
+      </h1>
+      <button className="increment-button" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
