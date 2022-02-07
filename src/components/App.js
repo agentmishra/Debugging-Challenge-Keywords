808096d00cce54cf48a804c559422bcf56963f40
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-02-08 02:00:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,12 @@ const App = () => {
   
   
 
-  render() {return (
+  return (
     <div className="ball">
       <h1 className="count" ondoubleclick={() => alert('cant edit it') }>{count}</h1>
       <button className="increment-button" onclick={() => setCount(count + 1) }>Increment</button>
     </div>
-  )}
+  )
 }
 
 
