1bc6b335f90907b1668d9fef6574a4f9b9aea182
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-02-08 01:55:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-//import React, { useEffect, useState } from "react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
@@ -8,12 +7,12 @@ const App = () => {
   
   
 
-  render() return (
+  render() {return (
     <div className="ball">
       <h1 className="count" ondoubleclick={() => { alert('cant edit it') }}>{count}</h1>
       <button className="increment-button" onclick={() => { setCount(count + 1) }}>Increment</button>
     </div>
-  )
+  )}
 }
 
 
